3c88c311c2d22f728aeb5e1bca5942fff4f294f7
unknown <domingos.neto@gmail.com>
2014-11-18 01:26:40 -0200

    Creating tutorial

---
+++ src/main/java/com/codeinstructions/rx/Test02.java
@@ -0,0 +1,7 @@
+package com.codeinstructions.rx;
+
+/**
+ * Created by Domingos on 17/11/2014.
+ */
+public class Test02 {
+}
---
+++ src/main/java/com/codeinstructions/rx/Test04.java
@@ -0,0 +1,44 @@
+package com.codeinstructions.rx;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test04{
+    public static void main(String[] args) {
+        testingSource(0, 100)
+                .subscribeOn(Schedulers.computation())
+                .subscribe(Test04::println);
+    }
+
+    private static Observable<Integer> testingSource(int min, int max) {
+        return Observable.create(subscriber -> {
+            for (int i = min; i < max; i++) {
+                if (subscriber.isUnsubscribed()) {
+                    break;
+                }
+                subscriber.onNext(i);
+            }
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    private static void println(int i) {
+        System.out.println(label() + i);
+    }
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss:SSS");
+
+    private static String time() {
+        return sdf.format(new Date(System.currentTimeMillis()));
+    }
+
+    private static String label() {
+        return time() + " [" + Thread.currentThread().getName() + "]: ";
+    }
+}
